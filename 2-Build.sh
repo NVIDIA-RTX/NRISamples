64ed34fc67c1533b846fcc55f096ba83163d1474
dzhdan <dzhdan@nvidia.com>
2025-04-16 10:04:55 +0800

    Scripts: use all available cores for building

--- 2-Build.sh
+++ 2-Build.sh
@@ -4,6 +4,6 @@ mkdir -p "_Build"
 
 cd "_Build"
 cmake ..
-cmake --build . --config Release -j$(nproc)
-cmake --build . --config Debug -j$(nproc)
+cmake --build . --config Release -j $(nproc)
+cmake --build . --config Debug -j $(nproc)
 cd ..
