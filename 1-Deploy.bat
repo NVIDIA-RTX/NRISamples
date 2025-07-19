1601f794af619d03f4be2670ec7b8cb672dcceeb
dzhdan <dzhdan@nvidia.com>
2025-07-19 12:49:25 +0800

    Fixed always successful GitHub action

--- 1-Deploy.bat
+++ 1-Deploy.bat
@@ -5,5 +5,8 @@ git submodule update --init --recursive
 mkdir "_Build"
 
 cd "_Build"
+
 cmake .. -A x64
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
 cd ..
--- 2-Build.bat
+++ 2-Build.bat
@@ -2,6 +2,11 @@
 @echo off
 
 cd "_Build"
+
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
 cmake --build . --config Debug -j %NUMBER_OF_PROCESSORS%
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+
 cd ..
